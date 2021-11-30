92075881a07efcac94f0d357948250f86b46e0b4
Luis Gomes <creativewild@users.noreply.github.com>
2021-11-30 01:19:53 +0000

    Build and dependencies
    added build to main

---
+++ Build/vorp_fishing.sql
@@ -0,0 +1,37 @@
+/* Bait Data*/
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_baitBread01x', 'Bread Bait', 10, 1, 'item_standard', 1);
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_baitCheese01x', 'Cheese Bait', 10, 1, 'item_standard', 1);
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_baitCorn01x', 'Corn Bait', 10, 1, 'item_standard', 1);
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_baitCricket01x', 'Cricket Bait', 10, 1, 'item_standard', 1);
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_FinisdFishlure01x', 'Small Fry Bait', 10, 1, 'item_standard', 1);
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_finishdcrawd01x', 'Crawfish Bait', 10, 1, 'item_standard', 1);
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES	('p_finishedragonfly01x', 'Dragonfly Bait', 10, 1, 'item_standard', 1);
+
+/* Fish Data*/
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishbluegil_01_ms', 'Blue gil (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishbluegil_01_sm', 'Blue gil (small)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishbullheadcat_01_ms', 'bullheadcat (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishbullheadcat_01_sm', 'bullheadcat (small)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishchainpickerel_01_ms', 'chain pickerel (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishchainpickerel_01_sm', 'chainpickerel (small)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishchannelcatfish_01_lg', 'channel catfish (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishchannelcatfish_01_xl', 'channel catfish (extra large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishlakesturgeon_01_lg', 'lake sturgeon (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishlargemouthbass_01_lg', 'large mouth bass (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishlargemouthbass_01_ms', 'large mouth bass (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishlongnosegar_01_lg', 'long nose gar (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishmuskie_01_lg', 'muskie (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishnorthernpike_01_lg', 'northern pike (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishperch_01_ms', 'perch (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishperch_01_sm', 'perch (small)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishrainbowtrout_01_lg', 'rainbow trout (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishrainbowtrout_01_ms', 'rainbow trout(medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishredfinpickerel_01_ms', 'red fin pickerel (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishredfinpickerel_01_sm', 'red fin pickerel (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishrockbass_01_ms', 'rock bass (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishrockbass_01_sm', 'rock bass (small)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishsalmonsockeye_01_lg', 'salmon sockeye (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishsalmonsockeye_01_ml', 'salmon sockeye (Legendary)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishsalmonsockeye_01_ms', 'salmon sockeye (medium)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishsmallmouthbass_01_lg', 'small mouth bass (large)', '10', 'item_standard', '1');
+INSERT INTO `items` (`item`, `label`, `limit`, `type`, `usable`) VALUES ('a_c_fishsmallmouthbass_01_ms', 'small mouth bass (medium)', '10', 'item_standard', '1');
---
+++ Build/vorp_fishing/fxmanifest.lua
@@ -0,0 +1,17 @@
+
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+
+client_scripts {
+    "client/vorp_fishing.net.dll"
+}
+
+server_scripts {
+	"server/vorp_fishing_sv.net.dll"
+}
+
+files {
+  'Newtonsoft.Json.dll',
+}
